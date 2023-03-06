096b800cded59b569801e5a2288fa6f9ca88f62e
Angel B <121882669+abrvg@users.noreply.github.com>
2023-03-06 14:46:52 -0600

    Add files
    Add palindrome check function

---
+++ A_PalindromeNumber.py
@@ -0,0 +1,33 @@
+"""
+Given an integer x, return true if x is palindrome
+
+Example 1
+input: x=121
+output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. 
+Therefore it is not a palindrome.
+
+Example 3
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+"""
+
+class Solution:
+    
+    #def __init__(self, x):
+    #    self.x = x
+    
+    def isPalindrome(self,x):
+        self.x = x
+        return print(str(self.x) == str(self.x)[::-1])
+
+x = 10
+s = Solution().isPalindrome(x)
+print(s)
