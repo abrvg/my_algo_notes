c56a431821f6ede06e707caf66dc5b323f742b68
Angel B <121882669+abrvg@users.noreply.github.com>
2024-05-08 12:10:40 -0600

    Create LC083_RemoveDuplicateSorted.py

---
+++ 001-100/LC083_RemoveDuplicateSorted.py
@@ -0,0 +1,31 @@
+"""
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        prev = None
+        current = head
+
+        while current:
+            if prev is None or prev.val != current.val:
+                prev = current
+            else:
+                current = current.next
+                prev.next = current
+        
+        return head
